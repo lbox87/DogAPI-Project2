0ff8a50d0093b8e608f77f7364d269f18d79b409
Andrew Kolb <akolb87@gmail.com>
2018-10-29 20:04:51 -0500

    endpoint rendered update

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function breedSubmitted() {
         //     alert("Please select a number 1 through 50.");
         //     return;
         // }
-        // fetchDogs();
+        fetchDogs();
     });
 }
 
@@ -28,25 +28,23 @@ function submittedBreed() {
     console.log(`current endpoint is ${endpoint}`);
 }
 
-// // get dog images from endpoint
-// function fetchDogs() {
-//     fetch(`${endpoint}`)
-//         .then(response => response.json())
-//         .then(responseJson => displayDog(responseJson))
-//         .catch(error => {
-//             console.log(error);
-//             alert('Something went wrong, check console.');
-//         });
+// get dog images from endpoint
+function fetchDogs() {
+    fetch(`${endpoint}`)
+        .then(response => response.json())
+        .then(responseJson => displayDog(responseJson))
+        .catch(error => {
+            console.log(error);
+            alert('Something went wrong, check console.');
+        });
 
-// }
+}
 
-// // append each dog image from the API response into the js-dogs div
-// function displayDog(responseJson) {
-//     console.log(responseJson);
-//     for (let i = 0; i < responseJson.message.length; i++) {
-//         $(`.js-dogs`).append(`<img src="${responseJson.message[i]}" class="col-3 results-img">`);
-//     }
-// }
+// append each dog image from the API response into the js-dogs div
+function displayDog(responseJson) {
+    console.log(responseJson);
+    $(`.js-dogs`).append(`<img src="${responseJson.message}" class="col-3 results-img">`);
+}
 
 // // refresh values and clear div
 // function clearValues() {
