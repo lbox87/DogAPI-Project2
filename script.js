6e34edf61fded1fe3820c7924d035ce020509e21
Andrew Kolb <akolb87@gmail.com>
2018-10-29 20:00:45 -0500

    updated button submit stores endpoint for breed selected

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
 'use strict';
 
-let numberSelected = "";
+let breedSelected = "";
 let endpoint = "";
 
-function numberSubmitted() {
+function breedSubmitted() {
     $(`.js-submit`).click(event => {
         event.preventDefault();
-        clearValues();
-        submittedNumber();
+        // clearValues();
+        submittedBreed();
         // prevent values other than 1-50
-        if ((numberSelected > 50) || (numberSelected < 1)) {
-            alert("Please select a number 1 through 50.");
-            return;
-        }
-        fetchDogs();
+        // if ((numberSelected > 50) || (numberSelected < 1)) {
+        //     alert("Please select a number 1 through 50.");
+        //     return;
+        // }
+        // fetchDogs();
     });
 }
 
 // captures number submitted, or defaults to 3 if nothing submitted.
-function submittedNumber() {
-    numberSelected = $('.js-number').val();
+function submittedBreed() {
+    breedSelected = $('.js-breed').val();
     // change empty submission to 3
-    if (numberSelected == "") {
-        numberSelected = 3;
-    }
-    endpoint = "https://dog.ceo/api/breeds/image/random/" + numberSelected;
+    // if (breedSelected == "") {
+    //     breedSelected = 3;
+    // }
+    endpoint = "https://dog.ceo/api/breed/" + breedSelected + "/images/random";
+    console.log(`current endpoint is ${endpoint}`);
 }
 
-// get dog images from endpoint
-function fetchDogs() {
-    fetch(`${endpoint}`)
-        .then(response => response.json())
-        .then(responseJson => displayDog(responseJson))
-        .catch(error => {
-            console.log(error);
-            alert('Something went wrong, check console.');
-        });
+// // get dog images from endpoint
+// function fetchDogs() {
+//     fetch(`${endpoint}`)
+//         .then(response => response.json())
+//         .then(responseJson => displayDog(responseJson))
+//         .catch(error => {
+//             console.log(error);
+//             alert('Something went wrong, check console.');
+//         });
 
-}
+// }
 
-// append each dog image from the API response into the js-dogs div
-function displayDog(responseJson) {
-    console.log(responseJson);
-    for (let i = 0; i < responseJson.message.length; i++) {
-        $(`.js-dogs`).append(`<img src="${responseJson.message[i]}" class="col-3 results-img">`);
-    }
-}
+// // append each dog image from the API response into the js-dogs div
+// function displayDog(responseJson) {
+//     console.log(responseJson);
+//     for (let i = 0; i < responseJson.message.length; i++) {
+//         $(`.js-dogs`).append(`<img src="${responseJson.message[i]}" class="col-3 results-img">`);
+//     }
+// }
 
-// refresh values and clear div
-function clearValues() {
-    numberSelected = "";
-    endpoint = "";
-    $(`.js-dogs`).html("");
-}
+// // refresh values and clear div
+// function clearValues() {
+//     numberSelected = "";
+//     endpoint = "";
+//     $(`.js-dogs`).html("");
+// }
 
-$(numberSubmitted);
+$(breedSubmitted);
